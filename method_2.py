340d6f649b8470253527f5bcb64bc3f01812b57d
DRC Recovery <85211068+DRCRecoveryData@users.noreply.github.com>
2023-09-14 01:04:47 +0700

    Update method_2.py
    optimized version

--- method_2.py
+++ method_2.py
@@ -14,45 +14,38 @@
 import os
 import struct
 
-img_path = input("Enter path to disk image file: ")
+def main():
+    img_path = input("Enter path to disk image file: ")
 
-# create "Patched" directory if it doesn't exist
-if not os.path.exists('Patched'):
-    os.mkdir('Patched')
+    # create "Patched" directory if it doesn't exist
+    os.makedirs('Patched', exist_ok=True)
 
-with open(img_path, 'r+b') as f:
-    # loop over all occurrences of the USBC sector in the file
-    while True:
-        # find the next occurrence of the USBC sector
-        usbc_offset = f.read().find(b'USBC')
-        if usbc_offset == -1:
-            # USBC sector not found, exit loop
-            break
+    with open(img_path, 'r+b') as f:
+        data = f.read()
+        usbc_offset = data.find(b'USBC')
 
-        # decode the USBC sector and determine the number of sectors it was intended to write
-        f.seek(usbc_offset)
-        data = bytearray(f.read(512))
-        cmd_len = struct.unpack('>H', data[22:24])[0]
+        while usbc_offset != -1:
+            cmd_len = struct.unpack_from('>H', data, usbc_offset + 22)[0]
 
-        # delete the USBC sector
-        f.seek(usbc_offset)
-        f.write(b'\x00' * 512)
+            # delete the USBC sector
+            data = data[:usbc_offset] + b'\x00' * 512 + data[usbc_offset + 512:]
 
-        # shift all sectors in the block up by one LBA
-        for i in range(2, cmd_len+1):
-            sector_offset = usbc_offset + i*512
-            f.seek(sector_offset)
-            sector_data = bytearray(f.read(512))
-            f.seek(sector_offset - 512)
-            f.write(sector_data)
+            # shift all sectors in the block up by one LBA
+            for i in range(2, cmd_len + 1):
+                sector_offset = usbc_offset + i * 512
+                data = data[:sector_offset - 512] + data[sector_offset:sector_offset + 512] + data[sector_offset - 512:]
 
-        # insert a zero-filled sector at the end of the block
-        end_offset = usbc_offset + cmd_len * 512
-        f.seek(end_offset)
-        f.write(b'\x00' * 512)
+            # insert a zero-filled sector at the end of the block
+            end_offset = usbc_offset + cmd_len * 512
+            data = data[:end_offset] + b'\x00' * 512 + data[end_offset:]
 
-    # save the patched disk image to the "Patched" directory
-    f.seek(0)
-    patched_img_path = os.path.join('Patched', os.path.basename(img_path))
-    with open(patched_img_path, 'wb') as patched_f:
-        patched_f.write(f.read())
+            usbc_offset = data.find(b'USBC', end_offset)
+
+        # save the patched disk image to the "Patched" directory
+        patched_img_path = os.path.join('Patched', os.path.basename(img_path))
+        with open(patched_img_path, 'wb') as patched_f:
+            patched_f.write(data)
+
+if __name__ == "__main__":
+    main()
+
